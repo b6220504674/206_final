b7eee0d7f3e8238e89b04da5673c1eaef2e8bc03
Teeraphat-4674 <teeraphat.wu@live.ku.th>
2021-03-29 19:09:41 +0700

    show Nearest Number

---
+++ Nearest Number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    unsigned long long n,i;
+    int c,j,k,m;
+    scanf("%d %d",&n,&c);
+    i=n%10;
+    if(i<c)
+    {
+    i=n%c;
+    j=10-i;
+    k=n+j;
+    }
+    else if(i>c)
+    {
+    j=i-c;
+    m=10-j;
+    k=n+m;
+    }
+    printf("%d",k);
+    
+    return 0;
+}
